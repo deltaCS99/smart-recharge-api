fefd544bdc3f2a8e626d3175e2b8fea5f79be4e0
deltaCS99 <kambangacraig@gmail.com>
2023-09-21 01:42:05 +0200

    feat(project-setup): vercel CD setup add dialectModule

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,6 +1,8 @@
 const Sequelize = require("sequelize")
 
-const sequelize = new Sequelize(`postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:5432/${process.env.POSTGRES_DATABASE}`)
+const sequelize = new Sequelize(`postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:5432/${process.env.POSTGRES_DATABASE}`, {
+    dialectModule: require("pg")
+  })
 
 const dbConnection = async () => {
     try {
